a5b8439bdd113545da4c7aeb01d3eeae31b7fb61
baikinjo <baikinjo.28@gmail.com>
2024-01-09 00:31:47 +0000

    data mod

--- src/data.ts
+++ src/data.ts
@@ -1,111 +1,114 @@
 interface Exercise {
-    exercise: string;
-    reps: string;
-    weight: number;
+  exercise: string;
+  reps: string;
+  weight: number;
+}
+
+export interface Day {
+  dayNumber: number;
+  plan: string;
+  day: string;
+  exercises: Exercise[];
+}
+
+export interface Program {
+  week: number;
+  days: Day[];
+}
+
+
+let last = 0;
+export const program: Program[] = [];
+
+const getWeight = (name: string, day: number): number => {
+  let start = day >= 29 ? 157.5 : 90;
+  let multiplier = day >= 29 ? 2.5 : 5;
+  switch (name) {
+    case "squat":
+      return 45 + day * 5;
+    case "bench_press":
+      return 45 + day * 2.5;
+    case "barbell_row":
+      return 65 + day * 2.5;
+    case "oh_press":
+      return 42.5 + day * 2.5;
+    case "deadlift":
+      return start + day * multiplier;
+    default:
+      throw Error(`Invalid Excercise name: ${name}`);
   }
-  
-  export interface Day {
-    dayNumber: number;
-    plan: string;
-    day: string;
-    exercises: Exercise[];
+};
+
+const getPlanName = (week: number, day: number): string => {
+  if (week % 2 === 0) {
+    return day % 2 === 0 ? "a" : "b";
+  } else {
+    return day % 2 === 0 ? "b" : "a";
   }
-  
-  export interface Program {
-    week: number;
-    days: Day[];
+};
+
+const getDayName = (day: number): string => {
+  if (day % 3 === 0) return "monday";
+  if (day % 3 === 1) return "wednesday";
+  if (day % 3 === 2) return "friday";
+  throw Error(`Invalid Day ${day}`);
+};
+
+const getExerciseName = (plan: string, day: number): string => {
+  if (day % 3 === 0) return "squat";
+  if (day % 3 === 1) return plan === "a" ? "bench_press" : "oh_press";
+  if (day % 3 === 2) return plan === "a" ? "barbell_row" : "deadlift";
+  throw Error(`Wrong plan or day: plan: ${plan}, day: ${day}`);
+};
+
+const getReps = (name: string): string => {
+  return name === "deadlift" ? "1x5" : "5x5";
+};
+
+for (let i = 0; i < 12; i++) {
+  const week = i + 1;
+  const days = [];
+  for (let j = 0; j < 3; j++) {
+    const plan = getPlanName(i, j);
+    const day = getDayName(j);
+    const exercises = [];
+    for (let k = 0; k < 3; k++) {
+      const exercise = getExerciseName(plan, k);
+      const reps = getReps(exercise);
+      const weight = getWeight(exercise, last);
+      exercises.push({
+        exercise,
+        reps,
+        weight
+      });
+    }
+    days.push({
+      dayNumber: last,
+      plan,
+      day,
+      exercises
+    });
+    last++;
   }
-  
-  export const program: Program[] = [];
-  
-  const getWeight = (name: string, day: number): number => {
-    const baseWeights: { [key: string]: number } = {
-      squat: 45,
-      bench_press: 45,
-      barbell_row: 65,
-      oh_press: 42.5,
-      deadlift: day >= 29 ? 157.5 : 90 + day * (day >= 29 ? 2.5 : 5),
-    };
-  
-    if (name in baseWeights) {
-      return (
-        baseWeights[name] +
-        day *
-          (name === "deadlift"
-            ? day >= 29
-              ? 2.5
-              : 5
-            : name === "bench_press"
-            ? 2.5
-            : 5)
-      );
-    } else {
-      throw new Error(`Invalid Exercise name: ${name}`);
-    }
-  };
-  
-  const getPlanName = (week: number, day: number): string => {
-    const planNames = ["a", "b"];
-    return planNames[(week % 2) * 1 + (day % 2)];
-  };
-  
-  const getDayName = (day: number): string => {
-    const dayNames = ["Monday", "Wednesday", "Friday"];
-    if (day >= 0 && day <= 2) {
-      return dayNames[day];
-    } else {
-      throw new Error(`Invalid Day ${day}`);
-    }
-  };
-  
-  const getExerciseName = (plan: string, day: number): string => {
-    const exercisesByDay: { [key: number]: string } = {
-      0: "squat",
-      1: plan === "a" ? "bench_press" : "oh_press",
-      2: plan === "a" ? "barbell_row" : "deadlift",
-    };
-    return exercisesByDay[day % 3];
-  };
-  
-  const getReps = (name: string): string => {
-    return name === "deadlift" ? "1x5" : "5x5";
-  };
-  
-  for (let week = 1; week <= 12; week++) {
-    const days: Day[] = [];
-  
-    for (let day = 0; day < 3; day++) {
-      const plan = getPlanName(week - 1, day);
-      const dayName = getDayName(day);
-      const exercises: Exercise[] = [];
-  
-      for (let exerciseIndex = 0; exerciseIndex < 3; exerciseIndex++) {
-        const exerciseName = getExerciseName(plan, exerciseIndex);
-        const reps = getReps(exerciseName);
-        const weight = getWeight(exerciseName, week * day);
-        exercises.push({ exercise: exerciseName, reps, weight });
-      }
-  
-      days.push({ dayNumber: week * day, plan, day: dayName, exercises });
-    }
-  
-    program.push({ week, days });
+  program.push({
+    week,
+    days
+  });
+}
+
+export const convertExerciseName = (name: string) => {
+  switch (name) {
+    case "squat":
+      return "Squat";
+    case "bench_press":
+      return "Bench Press";
+    case "barbell_row":
+      return "Barbell Row";
+    case "oh_press":
+      return "Overhead Press";
+    case "deadlift":
+      return "Deadlift";
+    default:
+      throw Error(`Unknown name: $name`);
   }
-  
-  export const convertExerciseName = (name: string) => {
-    switch (name) {
-      case "squat":
-        return "Squat";
-      case "bench_press":
-        return "Bench Press";
-      case "barbell_row":
-        return "Barbell Row";
-      case "oh_press":
-        return "Overhead Press";
-      case "deadlift":
-        return "Deadlift";
-      default:
-        throw Error(`Unknown name: $name`);
-    }
-  };
-  
+};
